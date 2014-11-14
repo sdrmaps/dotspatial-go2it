144b7645b62290ded1c6b6f3f15615d54094aab1
Justin Penka <jpenka@gmail.com>
2014-11-13 22:22:08 -0500

    removed year from global assembly info

--- core/AssemblyInfo/AssemblyInfoGlobal.cs
+++ core/AssemblyInfo/AssemblyInfoGlobal.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("SDR")]
-[assembly: AssemblyCopyright("Copyright © Spatial Data Research Inc. 2012")]
-[assembly: AssemblyTrademark("Spatial Data Research Inc. 2012")]
+[assembly: AssemblyCopyright("Copyright © Spatial Data Research Inc.")]
+[assembly: AssemblyTrademark("Spatial Data Research Inc.")]
 [assembly: AssemblyCulture("")]
 
 #if DEBUG
