a42aed67300171edba27a2baac8fb50e639d2c23
Justin Penka <jpenka@gmail.com>
2015-12-04 16:19:01 -0600

    Changed MdbHelper database calls to using methods

--- core/Components/SDR.Data/Database/MdbHelper.cs
+++ core/Components/SDR.Data/Database/MdbHelper.cs
@@ -11,21 +11,25 @@ namespace SDR.Data.Database
     /// </summary>
     public static class MdbHelper
     {
-
         /// <summary>
-        /// To get the full MDB connection string given the MDB database path
+        /// To get the full MDB connection string given the MDB database path,  and validates the connection
         /// </summary>
         public static string GetMdbConnectionString(string dbFileName)
         {
             if (String.IsNullOrEmpty(dbFileName))
-                throw new ArgumentException("dbFileName is null or empty.", dbFileName);
+                throw new ArgumentException("Database filename is null or empty.", dbFileName);
 
             var conn = new OleDbConnectionStringBuilder
             {
                 DataSource = dbFileName,
                 Provider = "Microsoft.Jet.Oledb.4.0"
             };
-            return conn.ConnectionString;
+
+            using (var cnn = new OleDbConnection(conn.ConnectionString))
+            {
+                cnn.Open();
+                return cnn.ConnectionString;
+            }
         }
 
         /// <summary>
@@ -33,8 +37,11 @@ namespace SDR.Data.Database
         /// This function returns false, if the mdb db
         /// file doesn't exist or if the file size is 0 Bytes
         /// </summary>
-        public static bool DatabaseExists(string dbPath)
+        public static bool DatabaseFileExists(string dbPath)
         {
+            if (String.IsNullOrEmpty(dbPath))
+                throw new ArgumentException("Database path is null or empty.", dbPath);
+
             if (!File.Exists(dbPath))
             {
                 return false;
@@ -51,17 +58,17 @@ namespace SDR.Data.Database
         /// <returns></returns>
         public static bool TableExists(string conn, string table)
         {
-            try
+            if (String.IsNullOrEmpty(conn))
+                throw new ArgumentException("Database connection string is null or empty.", conn);
+
+            if (String.IsNullOrEmpty(table))
+                throw new ArgumentException("Table name is null or empty.", table);
+
+            using (var cnn = new OleDbConnection(conn))
             {
-                var cnn = new OleDbConnection(conn);
                 cnn.Open();
                 var exists = cnn.GetSchema("Tables", new string[4] { null, null, table, "TABLE" }).Rows.Count > 0;
-                cnn.Close();
                 return exists;
-            }
-            catch
-            {
-                return false;
             }
         }
 
@@ -74,23 +81,25 @@ namespace SDR.Data.Database
         /// <returns></returns>
         public static bool FieldExists(string conn, string table, string field)
         {
-            try
-            {
-                var cnn = new OleDbConnection(conn);
-                cnn.Open();
+            if (String.IsNullOrEmpty(conn))
+                throw new ArgumentException("Database connection string is null or empty.", conn);
 
+            if (String.IsNullOrEmpty(table))
+                throw new ArgumentException("Table name is null or empty.", table);
+
+            if (String.IsNullOrEmpty(field))
+                throw new ArgumentException("Field name is null or empty.", field);
+
+            using (var cnn = new OleDbConnection(conn))
+            {
+                cnn.Open();
                 var dbTable = new DataTable();
                 var sqlString = "SELECT TOP 1 + FROM " + table;
                 var dbAdapter = new OleDbDataAdapter(sqlString, cnn);
                 dbAdapter.Fill(dbTable);
-
                 var i = dbTable.Columns.IndexOf(field);
                 return i != -1;
-            }
-            catch
-            {
-                return false;
-            }
+            }            
         }
     }
 }
