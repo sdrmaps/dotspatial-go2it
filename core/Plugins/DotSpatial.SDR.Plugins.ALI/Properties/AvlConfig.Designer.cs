e79eb431933fd6db9d0fd9b9e10ec3f2d8a7efdf
Justin Penka <jpenka@gmail.com>
2016-02-10 13:09:29 -0600

    moved generic settings file to proper location

--- core/Plugins/DotSpatial.SDR.Plugins.ALI/Properties/AvlConfig.Designer.cs
+++ core/Plugins/DotSpatial.SDR.Plugins.ALI/Properties/AvlConfig.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DotSpatial.SDR.Plugins.ALI {
+namespace DotSpatial.SDR.Plugins.ALI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
